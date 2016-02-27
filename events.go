b52d0bb21d498b505093a5101734572d24049b81
Rivalo <rivalo@home.nl>
2016-02-27 16:32:44 +0100

    discord-go has a pretty sweet ReplaceMentions function #7

--- events.go
+++ events.go
@@ -10,11 +10,13 @@ import (
 // This function will be called (due to above assignment) every time a new
 // message is created on any channel that the autenticated user has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
 	UserName := color.New(color.FgGreen).SprintFunc()
+	Text := color.New(color.FgWhite).SprintFunc()
 
 	if State.InsertMode {
 		if m.ChannelID == State.Channel.ID {
-			log.Printf("> %s > %s\n", UserName(m.Author.Username), m.Content)
+			log.Printf("> %s > %s\n", UserName(m.Author.Username), Text(m.ContentWithMentionsReplaced()))
 		}
 	}
 
